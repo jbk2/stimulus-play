d74aae1ce6b0e12b2cb9f6c95e12950c8fdd363b
jbk2 <james@humanbenefits.com>
2024-01-31 13:55:12 +0100

    Add Toggle Stimulus controller with #animate & #show_dropdown

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,17 +1,25 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["checkbox"]
+  static targets = ["checkbox", "checkedOutput", "select"]
   connect() {
     console.log("Toggle Controller is now connected.")
+    console.log(this.hasCheckboxTarget);
+    console.log(this.hasCheckedOutputTarget);
+    console.log(this.hasSelectTarget);
   }
 
   animate() {
     if (this.checkboxTarget.checked) {
-      this.checkboxTarget.classList.remove("animate-bounce");
+      this.checkedOutputTarget.innerHTML = `<span class="animate-spin inline-block">🍕</span>👍🏻` // this.checkboxTarget.classList.remove("animate-bounce");      
+      // this.checkedOutputTarget.classList.add("animate-bounce");
     } else {
-      this.checkboxTarget.classList.add("animate-bounce");
+      this.checkedOutputTarget.innerHTML = "" //   this.checkboxTarget.classList.add("animate-bounce");
     }
+  }
+
+  show_dropdown() {
+    this.selectTarget.classList.toggle("hidden")
   }
  
 }
